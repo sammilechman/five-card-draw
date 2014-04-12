5303542c73796e76b595b35002b71a41e72d2777
Sam Milechman <sammilechman@gmail.com>
2014-04-11 17:58:32 -0700

    Begin hand evaluation function

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,5 +1,7 @@
 class Hand
   attr_reader :set
+  FACES = [:two, :three, :four, :five, :six, :seven, :eight, :nine,
+      :ten, :jack, :queen, :king, :ace]
 
   def initialize
     @set = []
@@ -7,10 +9,12 @@ class Hand
 
   def draw_initial_hand(deck)
     self.get_cards(deck, 5)
+    self.sort_set!
   end
 
   def get_cards(deck, num)
     @set += deck.draw_cards(num)
+    self.sort_set!
   end
 
   def discard(*card_idxs)
@@ -18,4 +22,50 @@ class Hand
       set.delete_at(i)
     end
   end
+
+  def evaluate_hand
+
+  end
+
+  def pair
+  end
+
+  def two_pair
+  end
+
+  def three_of_kind
+  end
+
+  def straight
+    (0...(@set.count - 1)).each do |idx|
+      return false if @set[idx + 1].value - @set[idx].value != 1
+    end
+    return [:straight, highest_card(set)]
+  end
+
+  def flush
+    return [:flush, highest_card(set)] if set.all? do |card|
+      card.suit == set[0].suit
+    end
+    false
+  end
+
+  def full_house
+  end
+
+  def four_of_kind
+  end
+
+  def straight_flush
+
+  end
+
+  def sort_set!
+    @set.sort_by! { |card| card.value }
+  end
+
+  def highest_card(cards)
+    cards.last.face
+  end
+
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -43,9 +43,80 @@ describe Hand do
       expect(hand.set.include?(second_card)).to eq(false)
       expect(hand.set.include?(third_card)).to eq(false)
       expect(hand.set.include?(fifth_card)).to eq(false)
+    end
+  end
 
+  describe '#flush' do
+    it "should return [handtype, relavent] if hand is a flush" do
+      hand.instance_variable_set(:@set,
+        [Card.new(:ten, :hearts), Card.new(:eight, :hearts),
+        Card.new(:eight, :hearts), Card.new(:two, :hearts),
+        Card.new(:six, :hearts)])
+        hand.sort_set!
+        expect(hand.flush).to eq([:flush, :ten])
     end
 
+    it "should return false if not a flush" do
+      hand.instance_variable_set(:@set,
+        [Card.new(:ten, :hearts), Card.new(:eight, :clubs),
+        Card.new(:eight, :hearts), Card.new(:two, :hearts),
+        Card.new(:six, :hearts)])
+      hand.sort_set!
+      expect(hand.flush).to eq(false)
+    end
 
   end
+
+  describe '#straight' do
+    it "should return [handtype, relavent] if hand is a straight" do
+      hand.instance_variable_set(:@set,
+        [Card.new(:ten, :hearts), Card.new(:eight, :hearts),
+        Card.new(:nine, :hearts), Card.new(:jack, :hearts),
+        Card.new(:queen, :hearts)])
+      hand.sort_set!
+      expect(hand.straight).to eq([:straight, :queen])
+    end
+
+    it "should return false if not a straight" do
+      hand.instance_variable_set(:@set,
+        [Card.new(:ten, :hearts), Card.new(:jack, :clubs),
+        Card.new(:queen, :hearts), Card.new(:three, :hearts),
+        Card.new(:ace, :hearts)])
+      hand.sort_set!
+      expect(hand.straight).to eq(false)
+    end
+
+  end
+
+  describe '#evaluate_hand' do
+
+
+    it "should resolve a high card hand"
+      # hand.instance_variable_set(:@set,
+      #   [Card.new(:ten, :hearts), Card.new(:eight, :clubs),
+      #   Card.new(:four, :hearts), Card.new(:two, :clubs),
+      #   Card.new(:six, :hearts)])
+      # expect(hand.evaluate_hand).to eq([:high_card, :ten])
+
+
+    it "should resolve a pair hand"
+      # hand.instance_variable_set(:@set,
+      #   [Card.new(:ten, :hearts), Card.new(:eight, :clubs),
+      #   Card.new(:eight, :hearts), Card.new(:two, :clubs),
+      #   Card.new(:six, :hearts)])
+      # expect(hand.evaluate_hand).to eq([:pair, :eight])
+
+
+
+
+
+
+    # let(:set) { [Card.new(:ten, :hearts), Card.new(:eight, :clubs),
+    #   Card.new(:four, :hearts), Card.new(:five, :clubs),
+    #   Card.new(:six, :hearts)] }
+    it "should resolve an unknown hand"
+      #expect(hand.evaluate_hand).to eq([:high_card, :ten])
+
+  end
+
 end
