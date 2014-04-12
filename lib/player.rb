06db3b82d62ecbec6633ebe80b30f0f30d825c32
Sam Milechman <sammilechman@gmail.com>
2014-04-12 14:03:59 -0700

    Add Rspec tests for Player - see_bet, raise_bet, fold, init

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,11 @@
 class Player
 
-	attr_accessor :purse, :hand
+	attr_accessor :purse, :hand, :folded
 
 	def initialize(buy_in)
 		@purse = buy_in
 		@hand = nil
+		@folded = false
 	end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,8 +4,7 @@ describe Player do
 
 	subject(:player) { Player.new(100) }
 	
-	describe '#initialize' do
-		
+	describe '#initialize' do		
 		it 'should initialize with a purse equal to the buy-in' do
 			expect(player.purse).to eq(100)
 		end
@@ -14,7 +13,46 @@ describe Player do
 			expect(player.hand).to eq(nil)
 		end
 		
+		it 'should initialize with folded set to false' do
+			expect(player.folded).to eq(false)
+		end
+	end
+	
+	describe '#see_bet(current_bet)' do		
+		it 'not allow the player to have negative money, but allow them to see any bet' do
+			player.purse = 10
+			player.see_bet(20)
+			expect(player.purse).to eq(0)
+		end
 		
+		it 'should subtract the bet amount from the players purse' do
+			player.purse = 30
+			player.see_bet(20)
+			expect(player.purse).to eq(10)
+		end
+		
+	end
+	
+	describe '#raise_bet(current_bet)' do		
+		it 'automatically fold if they try and bet more than their purse' do
+			player.purse = 30
+			player.raise_bet(20)
+			expect(player.folded).to eq(true)
+		end
+		
+		it 'should subtract the bet amount from the players purse' do
+			player.purse = 50
+			player.raise_bet(20)
+			expect(player.purse).to eq(10)
+		end
+		
+	end
+	
+	describe '#fold' do		
+		it 'should set folded equal to true' do
+			player.fold
+			expect(player.folded).to eq(true)
+		end
 	end
 	
 	
